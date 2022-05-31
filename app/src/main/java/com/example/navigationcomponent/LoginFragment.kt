27c85b7361f09a254d6bc3cbaa6abaaa938a5f3a
Tehleel Mir <tehleelmir@Tehleels-MacBook-Pro.local>
2022-05-31 10:36:51 +0530

    done

--- app/src/main/java/com/example/navigationcomponent/LoginFragment.kt
+++ app/src/main/java/com/example/navigationcomponent/LoginFragment.kt
@@ -5,30 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.navigationcomponent.databinding.FragmentHomeBinding
+import com.example.navigationcomponent.databinding.FragmentLoginBinding
 
-class HomeFragment: Fragment() {
-    private lateinit var binding: FragmentHomeBinding
+class LoginFragment: Fragment() {
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentHomeBinding.inflate(inflater)
+        binding = FragmentLoginBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        init()
-    }
-
-    private fun init() {
-        binding.button.setOnClickListener {
-            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
-            findNavController().navigate(action)
-        }
     }
 }
